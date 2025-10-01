ed798fc3f5799b9bc0516468686df396e05bfcd8
Henry Yang <henryyang385@gmail.com>
2025-09-30 23:26:30 -0400

    Finish

--- climate-proof/.vite/deps/clsx.js
+++
@@ -1,9 +0,0 @@
-import {
-  clsx,
-  clsx_default
-} from "./chunk-6SD27EE3.js";
-import "./chunk-G3PMV62Z.js";
-export {
-  clsx,
-  clsx_default as default
-};
--- climate-proof/openapi-ts.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@hey-api/openapi-ts';
-
-export default defineConfig({
-  input: 'https://api.docs.riskthinking.ai/openapi-public.yml', // sign up at app.heyapi.dev
-  output: 'src/client',
-  plugins: [
-    '@tanstack/react-query',
-  ]
-});
